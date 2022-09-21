aa02be75609f9ccd31f1f79f9b6c2a75e9bfb384
giselamarque <110875368+giselamarque@users.noreply.github.com>
2022-09-21 18:26:45 -0500

    fixedmarvelapi

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,51 @@
 //Marvel Characters API URL
-// const marvelURL = "https://gateway.marvel.com:443/v1/public/characters?apikey=3bc4ed058d04fc848cf88cd88dd98142"; 
-// <>
-//  // Google search API URL
-//     <script async src="https://cse.google.com/cse.js?cx=f79ab53a4a2ab43ef">
-//     </script><div class="gcse-search"></div></>
-// let header= document.getElementById('header');
-// let characters=document.getElementById('characters');
-// let footer= document.getElementById('footer');
-// let string='';
-// async function getapi(marvelURL)
-//     // Storing response
-//     const response = await fetch(marvelURL)
 
-    fetch(marvelAPI).then(function(response) {
-        //checks if fetch request was successful
-        if (!response.ok) {
-            console.log(response); 
-            throw new Error('HTTP error, status = ' + response.status);
-            }
-        }).then(function)
-            // Returns the response as JSON
-            return response.json();
-            thenfunction(data)
-        // Logs the Marvel Characters API data
-            console.log('Marvel Universe Characters:', data);
+const marvelURL = "https://gateway.marvel.com:443/v1/public/characters?apikey=3bc4ed058d04fc848cf88cd88dd98142"; 
+<>
+// Google search API URL
+    <script async src="https://cse.google.com/cse.js?cx=f79ab53a4a2ab43ef">
+    </script><div class="gcse-search"></div></>
+
+let header= document.getElementById('header');
+
+let characters=document.getElementById('characters');
+
+let footer= document.getElementById('footer');
+
+let string='';
+
+async function getapi(marvelURL)
+
+// Storing response
+
+const response = await fetch(marvelURL)
+
+.then(function (response){
+    
+    // fetch request
+    if (!response.ok) {
+        throw new Error('HTTP error, status = ' + response.status);
+        }
+   
+        // Returns the response as JSON
+        return response.json();
+}).then(function(data){
+    
+    // Logs Marvel Characters API data
+        console.log('Marvel Universe Characters:', data);
+
+        // Display header
+        header.innerHTML='<div class="bg-red-900 text-center py-4 lg:px-4"><span class="font-semibold mr-2 text-left flex-auto text-white">Marvel Universe Characters</span></div>';
+        string +='<div class="flex flex-wrap pt-5">';
+        
+        // Loop the API data 
+        for(let i=0;i<data.data.results.length;i++){
+            let element = data.data.results[i];
+            //Display Each Character Attributes
+            string+='<div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 mb-4"><a href="'+element.urls[0].url+'" target="_blank"> <div class=" max-w-sm rounded overflow-hidden shadow-lg m-4"><img class="w-full" src="'+element.thumbnail.path+'/standard_small.'+element.thumbnail.extension+'" alt="'+element.name+'"><div class="px-6 py-4"><div class="font-bold text-xl mb-2">'+element.name+'</div></div></a></div></div>';
+        }
+        string+='</div'
+
+        characters.innerHTML=string;
+
+
