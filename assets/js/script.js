6b927ade31267c465e9528132a73ea6947d7dfbf
Emily Hill <emhillmed2021@gmail.com>
2022-09-24 17:27:41 -0500

    rough start to how to use the appended data, can format google later tonight

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,7 +55,15 @@ fetch(marvelUrl)
         var apendedName = data.results[i].name; 
         console.log(apendedName)
         $(".character-name").innerHTML = apendedName 
+
+        var comicAppearances = data.results[i].comics;
+        console.log(comicAppearances)
+        var comicDisplay = document.createElement("div")
+        comicDisplay = comicAppearances.items
+        $(".comics").append(comicDisplay)
         }
+
+
     })}
 
     else{
@@ -131,52 +139,4 @@ function secondHTML() {
 
 // characters.innerHTML=string;
 
-
-// let header= document.getElementById('header');
-
-// let characters=document.getElementById('characters');
-
-// let footer= document.getElementById('footer');
-
-// let string='';
-
-// async function getapi(marvelURL)
-
-// // Storing response
-
-// const response = await fetch(marvelURL)
-
-// .then(function (response){
     
-//     // fetch request
-//     if (!response.ok) {
-//         throw new Error('HTTP error, status = ' + response.status);
-//         }
-   
-//         // Returns the response as JSON
-//         return response.json();
-// }).then(function(data){
-    
-    // Log Marvel Characters API data
-        
-    // console.log('Marvel Universe Characters:', data);
-
-    //     // Displayed
-        
-    //     header.innerHTML='<div class="bg-red-900 text-center py-4 lg:px-4"><span class="font-semibold mr-2 text-left flex-auto text-white">Marvel Universe Characters</span></div>';
-    //     string +='<div class="flex flex-wrap pt-5">';
-        
-    //     // Loop the API data 
-        
-    //     for(let i=0;i<data.data.results.length;i++){
-    //         let element = data.data.results[i];
-            
-    //         //Display Character Attributes
-            
-    //         string+='<div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 mb-4"><a href="'+element.urls[0].url+'" target="_blank"> <div class=" max-w-sm rounded overflow-hidden shadow-lg m-4"><img class="w-full" src="'+element.thumbnail.path+'/standard_small.'+element.thumbnail.extension+'" alt="'+element.name+'"><div class="px-6 py-4"><div class="font-bold text-xl mb-2">'+element.name+'</div></div></a></div></div>';
-    //     }
-    //     string+='</div'
-
-    //     characters.innerHTML=string;
-
-    // });
