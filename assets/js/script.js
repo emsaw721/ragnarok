9942884382ec0398a7247240d87b22ea657a2473
Emily Hill <emhillmed2021@gmail.com>
2022-09-24 21:13:23 -0500

    getting closer to appending comic appearance text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
 
 //Marvel Characters API URL
 
+$(document).ready( function() {
+
+    
+    $("#secondpage").addClass("hidden") 
+
+
 $("#searchbtn").click(function() {
 var apiKey = "65ccc0475fbe74a76cd9393f0c51a7bd"
 var privateKey = "0b99ee2827d2a7cf3d74437bb5ec552cc183ef69"
@@ -11,14 +17,110 @@ var characterName = $("#textbox").val();
 var marvelUrl = "https://gateway.marvel.com/v1/public/characters?name=" +characterName+ "&ts=" +ts+ "&apikey=" +apiKey+ "&hash=" +hash
 console.log(marvelUrl)
 
+
+if (characterName === "") {
+    console.log("modal start")
+    var modal = document.getElementById("characterModal");
+    var span = document.getElementsByClassName("close")[0];
+
+
+        modal.style.display = "block";
+
+
+    $(span).click(function () {
+        modal.style.display = "none";
+    })
+
+    $(window).click(function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    })
+    return;
+
+}; 
+
+
 //fetch function
-fetch(marvelUrl)
-    .then(response => response.json())
-    .then(data => {
-        // data from the api
-        console.log(data);
-    }
-);
+
+$.ajax({
+    url: marvelUrl,
+    method: "GET"
+}).then(function(response){
+    console.log(response)
+    var data = response.data
+    console.log(data)
+
+    var results = data.results 
+            console.log(results)
+    
+            for (let i=0; i< results.length; i++ ) {
+            var appendedName = $("<div>").text(results[i].name); 
+            console.log(appendedName)
+            $(".character-name").append(appendedName)
+    
+            var comicAppearances = data.results[i].comics;
+            console.log(comicAppearances)
+
+            var comicDisplay = $("<div>").text(comicAppearances.items);
+            console.log(comicDisplay)
+            $(".comics").append(comicDisplay)
+
+            // for (let i=0; i< comicAppearances.length; i++) {
+            //     var comicDisplay = $("<div>").text(comicAppearances[i].items);
+            //     console.log(comicDisplay)
+            //     $(".comics").append(comicDisplay)
+            // }
+            } 
+})
+// fetch(marvelUrl)
+//     .then(function(response) {
+//         if (response.ok) {
+//             console.log(response)
+//         }
+//             else{
+//                 throw new Error('HTTP error, status = ' + response.status);
+//             }
+//         response.json()
+//     .then(data => {
+//         // data from the api
+//         console.log(data);
+
+//         var results = data.results 
+//         console.log(results)
+
+//         for (let i=0; i< results.length; i++ ) {
+//         var appendedName = $("<div>").text(results[i].name); 
+//         console.log(appendedName)
+//         $(".character-name").append(appendedName)
+
+//         var comicAppearances = data.results[i].comics;
+//         console.log(comicAppearances)
+//         var comicDisplay = document.createElement("div")
+//         comicDisplay = comicAppearances.items
+//         $(".comics").append(comicDisplay)
+//         } 
+
+
+//     })}
+
+// ); 
+
+
+  secondHTML(); 
+
+
+
+function secondHTML() {
+   
+  $("#firstpage").empty() 
+
+  $("#secondpage").removeClass("hidden") 
+    
+}
+
+
+})
 
 }); 
 
@@ -38,92 +140,37 @@ fetch(marvelUrl)
 
 // // Storing response
 
-//   var response = await fetch(marvelURL)
-// // don't want to use const because then can't change response variable 
+// var response = await fetch(marvelURL)
 
 // .then(function (response){
     
-//     // fetch request
-//     if (!response.ok) {
-//         throw new Error('HTTP error, status = ' + response.status);
-//         }
+// // fetch request
+// if (response.ok) {
+// throw new Error('HTTP error, status = ' + response.status);
+//       }
    
-//         // Returns the response as JSON
-//         return response.json();
-// }).then(function(data){
+//  // Returns the response as JSON
+//     return response.json();
+//  }).then(function(data){
     
-//     // Logs Marvel Characters API data
-//         console.log('Marvel Universe Characters:', data);
+//  // Logs Marvel Characters API data
+//   console.log('Marvel Universe Characters:', data);
 
-        // // Display header
-        // header.innerHTML='<div class="bg-red-900 text-center py-4 lg:px-4"><span class="font-semibold mr-2 text-left flex-auto text-white">Marvel Universe Characters</span></div>';
-        // string +='<div class="flex flex-wrap pt-5">';
+// // Display header
+//     header.innerHTML='<div class="bg-red-900 text-center py-4 lg:px-4"><span class="font-semibold mr-2 text-left flex-auto text-white">Marvel Universe Characters</span></div>';
+//     string +='<div class="flex flex-wrap pt-5">';
         
-        // // Loop the API data 
-        // for(let i=0;i<data.data.results.length;i++){
-        //     let element = data.data.results[i];
-        //     //Display Each Character Attributes
-        //     string+='<div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 mb-4"><a href="'+element.urls[0].url+'" target="_blank"> <div class=" max-w-sm rounded overflow-hidden shadow-lg m-4"><img class="w-full" src="'+element.thumbnail.path+'/standard_small.'+element.thumbnail.extension+'" alt="'+element.name+'"><div class="px-6 py-4"><div class="font-bold text-xl mb-2">'+element.name+'</div></div></a></div></div>';
-        // }
-        // string+='</div'
+// // Loop the API data 
 
-        // characters.innerHTML=string;
-=======
+// for(let i=0;i<data.data.results.length;i++){
+//  let element = data.data.results[i];
+ 
+//  //Display Each Character Attributes
 
-//Marvel Characters API URL
+//  string+='<div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 mb-4"><a href="'+element.urls[0].url+'" target="_blank"> <div class=" max-w-sm rounded overflow-hidden shadow-lg m-4"><img class="w-full" src="'+element.thumbnail.path+'/standard_small.'+element.thumbnail.extension+'" alt="'+element.name+'"><div class="px-6 py-4"><div class="font-bold text-xl mb-2">'+element.name+'</div></div></a></div></div>';
+// }
+//  string+='</div'
 
-const marvelURL = "https://gateway.marvel.com:443/v1/public/characters?apikey=3bc4ed058d04fc848cf88cd88dd98142"; 
-<>
-// Google search API URL
-    <script async src="https://cse.google.com/cse.js?cx=f79ab53a4a2ab43ef">
-    </script><div class="gcse-search"></div></>
+// characters.innerHTML=string;
 
-let header= document.getElementById('header');
-
-let characters=document.getElementById('characters');
-
-let footer= document.getElementById('footer');
-
-let string='';
-
-async function getapi(marvelURL)
-
-// Storing response
-
-const response = await fetch(marvelURL)
-
-.then(function (response){
     
-    // fetch request
-    if (!response.ok) {
-        throw new Error('HTTP error, status = ' + response.status);
-        }
-   
-        // Returns the response as JSON
-        return response.json();
-}).then(function(data){
-    
-    // Log Marvel Characters API data
-        
-    console.log('Marvel Universe Characters:', data);
-
-        // Displayed
-        
-        header.innerHTML='<div class="bg-red-900 text-center py-4 lg:px-4"><span class="font-semibold mr-2 text-left flex-auto text-white">Marvel Universe Characters</span></div>';
-        string +='<div class="flex flex-wrap pt-5">';
-        
-        // Loop the API data 
-        
-        for(let i=0;i<data.data.results.length;i++){
-            let element = data.data.results[i];
-            
-            //Display Character Attributes
-            
-            string+='<div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 mb-4"><a href="'+element.urls[0].url+'" target="_blank"> <div class=" max-w-sm rounded overflow-hidden shadow-lg m-4"><img class="w-full" src="'+element.thumbnail.path+'/standard_small.'+element.thumbnail.extension+'" alt="'+element.name+'"><div class="px-6 py-4"><div class="font-bold text-xl mb-2">'+element.name+'</div></div></a></div></div>';
-        }
-        string+='</div'
-
-        characters.innerHTML=string;
-
-        
-
